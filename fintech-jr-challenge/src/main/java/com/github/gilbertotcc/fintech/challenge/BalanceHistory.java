62c37b227d6c223984e3d35c95409a5a6550302d
Ephrem Mekonnen <ephrem.tibebe2010@gmail.com>
2021-11-15 13:46:58 +0100

    first commit

---
+++ fintech-jr-challenge/src/main/java/com/github/gilbertotcc/fintech/challenge/BalanceHistory.java
@@ -0,0 +1,57 @@
+package com.github.gilbertotcc.fintech.challenge;
+
+import io.vavr.collection.List;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.With;
+import lombok.*;
+
+@AllArgsConstructor
+@With(value = AccessLevel.PRIVATE)
+public class BalanceHistory {
+
+	private final Balance referenceBalance;
+	final List<Balance> balances = List.empty();
+
+	public BalanceHistory addTransaction(Transaction transaction) {
+
+		if (transaction.getBookedDate().isAfter(referenceBalance.getDate())) {
+			// Add the amount of the transaction on the referenced balance
+			var newAmount = referenceBalance.getAmount().add(transaction.getAmount());
+			balances.append(Balance.of(transaction.getBookedDate(), newAmount));
+		}
+
+		else if (transaction.getBookedDate().isBefore(referenceBalance.getDate())) {
+			// Subtract the amount of the transaction from the referenced balance
+
+			var newAmount = referenceBalance.getAmount().subtract(transaction.getAmount());
+			balances.append(Balance.of(transaction.getBookedDate(), newAmount));
+		}
+
+		else {
+			var newAmount = referenceBalance.getAmount().add(transaction.getAmount());
+			int index = IntStream.range(0, balances.size())
+					.filter(i -> balances.get(i).equals(transaction.getBookedDate())).findFirst().orElse(-1);
+
+			balances.append(balances.get(index).withAmount(newAmount));
+
+		}
+
+		return this;
+	}
+
+	public List<Balance> getBalances() {
+
+		if (balances.isEmpty()) {
+			balances.append(referenceBalance);
+
+		}
+
+		return balances;
+	}
+
+}
